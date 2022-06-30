5c5e1c12d04c90c08b529699563b17106e53c15f
TOMASZ_REJ <tomasz.rej@gopuff.com>
2022-06-30 17:45:23 +0200

    remove title section

--- App.js
+++ App.js
@@ -26,19 +26,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({children}): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
       <Text
         style={[
           styles.sectionDescription,
